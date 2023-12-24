0800ec7b90fac776b51339c9003d2ba8f6a38314
Norman Weekes <weekeswill@yahoo.com>
2023-12-24 10:12:41 -0800

    Updated travis and scripts/test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-#test
+# test
 
 set -e
-set -x
+# set -x  # Uncomment this line for debugging
 
-pytest --cov=samplemod --cov=tests --cov-report=term-missing ${@}
+pytest --cov=samplemod --cov=tests --cov-report=term-missing
+
+# Run lint script
 bash ./scripts/lint.sh
